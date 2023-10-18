d9856d40fe941887170efb7a885b6a6d91e2e3c2
abr-ya <gamer05@list.ru>
2023-10-18 22:22:04 +0300

    feat: Add Draw signs method

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 WIN_SIZE = 600
 CELL_SIZE = WIN_SIZE // 3
 INF = float('inf')
+vec2 = pg.math.Vector2
+CELL_CENTER = vec2(CELL_SIZE / 2)
 
 class TicTacToe:
   def __init__(self, game):
@@ -12,18 +14,47 @@ class TicTacToe:
     self.O_image = self.get_scaled_image(path='img/o.png', res=[CELL_SIZE] * 2)
     self.X_image = self.get_scaled_image(path='img/x.png', res=[CELL_SIZE] * 2)
 
+    self.game_array = [[INF, INF, INF],
+                      [INF, INF, INF],
+                      [INF, INF, INF]]
+    self.player = 1 # стартовый/текущий игрок
+
+    self.winner = None
+    self.game_steps = 0
+
+  def  draw_signs(self):
+    for y, row in enumerate(self.game_array):
+      for x, sign in enumerate(row):
+        if sign != INF:
+          self.game.screen.blit((self.X_image, self.O_image)[sign], vec2(x, y) * CELL_SIZE)
+
   def draw(self):
     self.game.screen.blit(self.field_image, (0, 0))
-    # self.draw_objects()
+    self. draw_signs()
     # self.draw_winner()
 
   @staticmethod
   def get_scaled_image(path, res):
     img = pg.image.load(path)
     return pg.transform.smoothscale(img, res)
+  
+  # делаем ход!)
+  def run_game_process(self):
+    current_cell = vec2(pg.mouse.get_pos()) // CELL_SIZE
+    col, row = map(int, current_cell)
+    left_click = pg.mouse.get_pressed()[0]
+
+    if left_click and self.game_array[row][col] == INF and not self.winner:
+      self.game_array[row][col] = self.player
+      self.player = not self.player
+
+  def print_caption(self):
+    pg.display.set_caption(f'Player "{("X", "O")[self.player]}" turn!')
 
   def run(self):
+    self.print_caption()
     self.draw()
+    self.run_game_process()
 
 class Game:
   def __init__(self):
